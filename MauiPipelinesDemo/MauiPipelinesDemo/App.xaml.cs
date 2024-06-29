af00c14068cc566f78eaa688754827bf0d787a04
Christian Strydom <christian.strydom@gmail.com>
2024-06-29 12:44:11 +0200

    Add sample MAUI app and pipeline script to build iOS, Android and Windows MAUI apps

---
+++ MauiPipelinesDemo/MauiPipelinesDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPipelinesDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
